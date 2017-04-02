ef94926d493d521ef71fead66d496c91e7737398
Benjamin Petrosky <benpetrosky@hotmail.com>
2017-04-02 13:59:19 -0700

    add dryer code to html, styles and script. As well as some light content and styles changes.

--- js/scripts.js
+++ js/scripts.js
@@ -2,66 +2,38 @@ $(document).ready(function(){
   $("#formOne").submit(function(event){
   event.preventDefault();
 
-
-  // Get the result from the first question.
   var q1Result = $("input[name=question1]:checked", "#formOne").val();
   var q2Result = $("input[name=question2]:checked", "#formOne").val();
   var q3Result = $("input[name=question3]:checked", "#formOne").val();
 
-  if (q1Result === "option3")
-  {
-    $("#notEpicodusDiv").show();
-  }
-  else if (q2Result === "option1" && q3Result !== "option1")
-  {
-    $("#track1").show();
-
-  }
-
-  else if (q2Result === "option2" && q3Result !== "option2")
-  {
-    $("#track2").show();
-
-  }
-  else if (q2Result === "option3" && q3Result !== "option3")
-  {
-    $("#track3").show();
-  }
-// everything above is a perfect schedule and fit.  Below is a good fit with a schedule conflict.
-
-  else if (q2Result === "option1" && q3Result === "option1")
-  {
-    $("#java-schedule-issue").show();
-
-  }
-
-  else if (q2Result === "option2" && q3Result === "option2")
-  {
-    $("#csharp-schedule-issue").show();
-
-  }
-  else if (q2Result === "option3" && q3Result === "option3")
-  {
-    $("#ruby-schedule-issue").show();
-
-  }
+    if (q1Result === "option3")
+    {
+      $("#notEpicodusDiv").show();
+    }
+    else if (q2Result === "option1" && q3Result !== "option1")
+    {
+      $("#track1").show();
+    }
+    else if (q2Result === "option2" && q3Result !== "option2")
+    {
+      $("#track2").show();
+    }
+    else if (q2Result === "option3" && q3Result !== "option3")
+    {
+      $("#track3").show();
+    }
+  // everything above is a perfect schedule and fit.  Below is a good fit with a schedule conflict.
+    else if (q2Result === "option1" && q3Result === "option1")
+    {
+      $("#java-schedule-issue").show();
+    }
+    else if (q2Result === "option2" && q3Result === "option2")
+    {
+      $("#csharp-schedule-issue").show();
+    }
+    else if (q2Result === "option3" && q3Result === "option3")
+    {
+      $("#ruby-schedule-issue").show();
+    }
+  });
 });
-});
-
-//
-// function myFunc(name)
-// {
-//   return name.length;
-// }
-//
-// var ryan = "Ryan";
-// var ben = "Ben";
-//
-// if (myFunc(ryan) < myFunc(ben))
-// {
-//   // DO something
-// }
-// else
-// {
-//   // Do something else
-// }
